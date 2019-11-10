77ce5bea921d5b3b698c5759029f1eae23ebb6ec
woodsdm <woodsdm0@gmail.com>
2019-11-09 19:18:56 -0600

    Some of DisplayRecipe's xml and .java are done

--- app/src/main/java/com/example/recipeapp/DisplayRecipe.java
+++ app/src/main/java/com/example/recipeapp/DisplayRecipe.java
@@ -1,9 +1,12 @@
 package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DisplayRecipe extends AppCompatActivity {
 
@@ -13,9 +16,12 @@ public class DisplayRecipe extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_recipe);
+
         Intent intent = getIntent();
         mRecipe = intent.getParcelableExtra(RECIPE_KEY);
 
-        setContentView(R.layout.activity_display_recipe);
+        TextView mRecipeNameHead = (TextView) findViewById(R.id.recipeNameHeading);
+        mRecipeNameHead.setText(mRecipe.getRecipeName());
     }
 }
