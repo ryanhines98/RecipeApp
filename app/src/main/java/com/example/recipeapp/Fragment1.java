87123e3d980ce0200097980a613eeb7790207e66
taekweon <taehkweon@gmail.com>
2019-11-11 20:16:13 -0600

    skeleton for the steps

---
+++ app/src/main/java/com/example/recipeapp/Fragment1.java
@@ -0,0 +1,33 @@
+package com.example.recipeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment1_layout, container, false);
+    }
+
+}
